0b786d89f12fa6a0f9d1bd1c08647fb0a3554cd1
Salmin Lev <prostoweb123@gmail.com>
2024-09-16 00:25:42 +0600

    Fix styles

--- www/app/views/layout/header.php
+++ www/app/views/layout/header.php
@@ -4,6 +4,5 @@
     <title>Simple Bootstrap Page</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/styles.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
